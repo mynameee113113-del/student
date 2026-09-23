27fe2773800e3772629b5a85de858be155e6d4a3
student <student@naver.com>
2026-09-23 16:15:52 +0900

    oracle , StudentDTO created

---
+++ src/main/java/com/kedu/dto/StudentDTO.java
@@ -0,0 +1,53 @@
+package com.kedu.dto;
+
+public class StudentDTO {
+
+	private int id;
+	private String name;
+	private int kor;
+	private int eng;
+	private int math;
+	
+	public StudentDTO() {
+		
+	}
+	public StudentDTO(int id, String name , int kor, int eng, int math) {
+		this.id = id;
+		this.name = name ;
+		this.kor = kor;
+		this.eng = eng;
+		this.math = math;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void SetId(int id) {
+		this.id = id ;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getKor() {
+		return kor;
+	}
+	public void setKor(int kor) {
+		this.kor = kor;
+	}
+	public int getEng() {
+		return eng;
+	}
+	public void setEng(int eng) {
+		this.eng = eng;
+	}
+	public int getMath() {
+		return math;
+	}
+	public void setMath(int math) {
+		this.math = math;
+	}
+}
